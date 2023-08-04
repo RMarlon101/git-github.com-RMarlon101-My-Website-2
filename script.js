ad502b0718563d00e0cafc9b92d0fd0d4c9e49c4
RMarlon101 <rashawn18288@gmail.com>
2023-08-04 14:42:23 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let time = 0;
+let counterElement = document.getElementById("time");
+
+function updateCounter(){
+    time++;
+    counterElement.textContent = time;
+}
+
+setInterval(updateCounter, 60000);
